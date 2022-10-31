66bfc8cbd97797edd1d093e2699ba29cd958278e
kretopaz <krittaphas.wi@depa.or.th>
2022-10-31 11:30:26 +0700

    update: depa_signature -> version 1.3.4

--- depa_signature/__manifest__.py
+++ depa_signature/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Signature',
-    'version': '1.3.3',
+    'version': '1.3.4',
 
     # any module necessary for this one to work correctly
     'depends': [
